7ca86d4b03edad7d495d14b8b3f399f295c22bde
Schreder <schkylj@dunwoody.edu>
2018-01-13 16:10:34 -0600

    Add project files.

---
+++ CWEB2010Lab1Try2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWEB2010Lab1Try2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
